7df303f54ed83cf885e8a438de85f95882342165
HeyDadCoolWhip <travis.yeet.ah@gmail.com>
2023-05-19 01:08:27 -0700

    base (some: .eslintrc.json, .lintstagedrc.js, .prettierrc.json, globals.css, layout.tsx, next.config.js, package.json, page.tsx, tauri.conf.json, versions.json, yarn.lock): Deleted: .eslintrc.json, .lintstagedrc.js, and .prettierrc.json. Changed: globals.css, layout.tsx (barely), next.config.js (barely), package.json (removed packages), versions.json (Don't know why), yarn.lock (yarn auto-changes)

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-'use client'
 import React from 'react';
 import './styles.css';
 
@@ -6,8 +5,75 @@ function Home() {
   return (
     <>
       <div className={"container"}>
-        <h1>hello</h1>
-        <p>hi</p>
+        <p id={"fairWarning"} >Fair warning: this was all stolen. This shit is fuckin hideous</p>
+        <div className={"timeline"}>
+          <div className={"timelineBox"} id={"primary"}>
+            <div className={"statusText"}>
+              <b className={"status"}>Type: 1</b>
+            </div>
+            <div className={"timelineBody"}>
+              <h4 className={"header"}><span className={"badge"}>primary</span></h4>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sem fringilla ut morbi tincidunt augue. Congue nisi vitae suscipit tellus mauris a diam. Mi ipsum faucibus vitae aliquet. Felis eget nunc lobortis mattis aliquam faucibus purus. Lobortis scelerisque fermentum dui faucibus in ornare quam viverra.</p>
+              <p className={"time"}>1 hour(s) ago</p>
+            </div>
+          </div>
+
+          <div className={"timelineBox"} id={"success"}>
+            <div className={"statusText"}>
+              <b className={"status"}>Type: 2</b>
+            </div>
+            <div className={"timelineBody"}>
+              <h4 className={"header"}><span className={"badge"}>success</span></h4>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sem fringilla ut morbi tincidunt augue. Congue nisi vitae suscipit tellus mauris a diam. Mi ipsum faucibus vitae aliquet. Felis eget nunc lobortis mattis aliquam faucibus purus. Lobortis scelerisque fermentum dui faucibus in ornare quam viverra.</p>
+              <p className={"time"}>3 hours ago</p>
+            </div>
+          </div>
+
+          <div className={"timelineBox"} id={"danger"}>
+            <div className={"statusText"}>
+              <b className={"status"}>Type: 3</b>
+            </div>
+            <div className={"timelineBody"}>
+              <h4 className={"header"}><span className={"badge"}>danger</span></h4>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sem fringilla ut morbi tincidunt augue. Congue nisi vitae suscipit tellus mauris a diam. Mi ipsum faucibus vitae aliquet. Felis eget nunc lobortis mattis aliquam faucibus purus. Lobortis scelerisque fermentum dui faucibus in ornare quam viverra.</p>
+              <p className={"time"}>8 hours ago</p>
+            </div>
+          </div>
+
+          <div className={"timelineBox"} id={"secondary"}>
+            <div className={"statusText"}>
+              <b className={"status"}>Type: 4</b>
+            </div>
+            <div className={"timelineBody"}>
+              <h4 className={"header"}><span className={"badge"}>secondary</span></h4>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sem fringilla ut morbi tincidunt augue. Congue nisi vitae suscipit tellus mauris a diam. Mi ipsum faucibus vitae aliquet. Felis eget nunc lobortis mattis aliquam faucibus purus. Lobortis scelerisque fermentum dui faucibus in ornare quam viverra.</p>
+              <p className={"time"}>1 day(s) ago</p>
+            </div>
+          </div>
+
+          <div className={"timelineBox"} id={"warning"}>
+            <div className={"statusText"}>
+              <b className={"status"}>Type: 5</b>
+            </div>
+            <div className={"timelineBody"}>
+              <h4 className={"header"}><span className={"badge"}>warning</span></h4>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sem fringilla ut morbi tincidunt augue. Congue nisi vitae suscipit tellus mauris a diam. Mi ipsum faucibus vitae aliquet. Felis eget nunc lobortis mattis aliquam faucibus purus. Lobortis scelerisque fermentum dui faucibus in ornare quam viverra.</p>
+              <p className={"time"}>1 day(s) & 2 hours ago</p>
+            </div>
+          </div>
+
+          <div className={"timelineBox"} id={"info"}>
+            <div className={"statusText"}>
+              <b className={"status"}>Type: 6</b>
+            </div>
+            <div className={"timelineBody"}>
+              <h4 className={"header"}><span className={"badge"}>info</span></h4>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sem fringilla ut morbi tincidunt augue. Congue nisi vitae suscipit tellus mauris a diam. Mi ipsum faucibus vitae aliquet. Felis eget nunc lobortis mattis aliquam faucibus purus. Lobortis scelerisque fermentum dui faucibus in ornare quam viverra.</p>
+              <p className={"time"}>2 days ago</p>
+            </div>
+          </div>
+
+        </div>
       </div>
     </>
   )
--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  output: "export",
   // Note: This feature is required to use NextJS Image in SSG mode.
   // See https://nextjs.org/docs/messages/export-image-api for different workarounds.
   images: {
